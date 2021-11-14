50ea2095a985e586bd738bd657ce92276d570a49
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-11-15 03:00:36 +0800

    auto load room list when switch account

--- app/src/app/tab3/tab3.page.ts
+++ app/src/app/tab3/tab3.page.ts
@@ -1,9 +1,9 @@
 import {Component} from '@angular/core'
 import {AlertController} from '@ionic/angular'
 import {AccountService} from '../account.service'
-import {APIResponse, MyRoomListResponse} from "../../types";
-import {RoomService} from "../room.service";
-import {NoticeService} from "../notice.service";
+import {APIResponse, MyRoomListResponse} from '../../types'
+import {RoomService} from '../room.service'
+import {NoticeService} from '../notice.service'
 
 @Component({
   selector: 'app-tab3',
@@ -27,17 +27,17 @@ export class Tab3Page {
 
   ionViewDidEnter() {
     this.token = this.accountService.token
-    this.roomService.getMyRooms().then(json => {
+    return this.loadRooms()
+  }
+
+  loadRooms() {
+    return this.roomService.getMyRooms().then(json => {
       if (json.error) {
         return this.noticeService.showError('Failed to load my rooms', json.error)
       } else {
         this.myRooms = json.rooms
       }
     })
-  }
-
-  getRoomUrl(room: MyRoomListResponse['rooms'][number]) {
-    return ['/room', {code: room.code}]
   }
 
   canSave() {
@@ -53,6 +53,7 @@ export class Tab3Page {
         {
           text: 'Change Token', handler: () => {
             this.accountService.token = this.token
+            return this.loadRooms()
           }
         }
       ]
@@ -77,24 +78,23 @@ export class Tab3Page {
     await alert.present()
   }
 
-  deleteRoom(room: MyRoomListResponse['rooms'][number], ) {
+  deleteRoom(room: MyRoomListResponse['rooms'][number]) {
     return this.alertController.create({
       header: 'Confirm to delete room?',
       message: 'Room ' + room.name + ' will be deleted',
       buttons: [
         {text: 'Cancel', role: 'cancel'},
         {
-          text: 'Delete Room', handler: () => {
-            return this.roomService.deleteRoom(room.id)
-              .then((json: APIResponse) => {
-                if (json.error) {
-                  return this.noticeService.showError('Failed to delete room', json.error)
-                } else {
-                  this.myRooms.splice(this.myRooms.indexOf(room))
-                  return this.noticeService.showSuccess('Deleted room')
-                }
-              })
-          }
+          text: 'Delete Room',
+          handler: () => this.roomService.deleteRoom(room.id)
+            .then((json: APIResponse) => {
+              if (json.error) {
+                return this.noticeService.showError('Failed to delete room', json.error)
+              } else {
+                this.myRooms.splice(this.myRooms.indexOf(room))
+                return this.noticeService.showSuccess('Deleted room')
+              }
+            })
         }
       ]
     })
