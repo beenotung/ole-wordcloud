958e99883415799e2b614f9e30ba0af33a454592
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-08-29 15:25:19 +0800

    devop: update deployment target

--- app/scripts/deploy.sh
+++ app/scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 set -o pipefail
-rsync -SavLP www hkit.cc:~/workspace/github.com/beenotung/ole-wordcloud/app
+rsync -SavLP www node@ole-wordcloud.hkit.cc:~/workspace/github.com/beenotung/ole-wordcloud/app
